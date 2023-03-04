899daf8c9684e917618fdd1e930784ad6a923148
KAYT33N <K80arabi@gmail.com>
2023-03-04 10:36:01 +0330

    added recoil and made databaseAtom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { useRecoilState } from 'recoil';
+import dataAtom from './Atoms/dataAtom';
+
 
 function App() {
+  const [shouldSetup, setShouldSetup] = useState(true)
+  const [data,setData] = useRecoilState(dataAtom)
+
   return (
     <h1>new app</h1>
   );
 }
 
-export default App;
+export default App
