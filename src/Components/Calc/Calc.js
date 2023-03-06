316ee5d79f0c2f1f6601f1b5251da6b3631eece6
KAYT33N <K80arabi@gmail.com>
2023-03-07 02:02:49 +0330

    sugar and shakur were weong

--- src/Components/Calc/Calc.js
+++ src/Components/Calc/Calc.js
@@ -10,15 +10,15 @@ function Calc(props){
     let uneavens = [];
     for (let i = 0; i < userCount; i++) {
         for (let j = 0; j < userCount; j++) {
-            if( i != j ){
+            if( i !== j ){
                 let talab_i_az_j = data.records
                     .filter(item=>{
-                        return item.sugar == i && item.shakur == j
+                        return item.sugar === i && item.shakur === j
                     })
                     .reduce((a,b)=> a+b.amount , 0)
                 let bedehi_i_be_j = data.records
                     .filter(item=>{
-                        return item.sugar == j && item.shakur == i
+                        return item.sugar === j && item.shakur === i
                     })
                     .reduce((a,b)=> a+b.amount , 0)
                 uneavens.push({i:i, j:j, amount:(bedehi_i_be_j - talab_i_az_j)})
@@ -29,7 +29,7 @@ function Calc(props){
     return (
         <ul>
             {uneavens
-            .filter(item=> item.amount>0)
+            .filter(item => item.amount>0)
             .map(item=>{
                 return (<>
                     <li>
